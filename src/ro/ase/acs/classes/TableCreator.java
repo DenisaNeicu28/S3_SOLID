cb709dab61a77a4f71aa81a02490c11fa0b15980
Denisa Neicu <deny.neicu@gmail.com>
2023-03-10 21:18:48 +0200

    first commit

---
+++ src/ro/ase/acs/classes/TableCreator.java
@@ -0,0 +1,21 @@
+package ro.ase.acs.classes;
+
+import ro.ase.acs.interfaces.Creator;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TableCreator implements Creator {
+    public void createTable(Connection connection) throws SQLException {
+        String sqlDrop = "DROP TABLE IF EXISTS employees";
+        String sqlCreate = "CREATE TABLE employees(id INTEGER PRIMARY KEY,"
+                + "name TEXT, address TEXT, salary REAL)";
+
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(sqlDrop);
+        statement.executeUpdate(sqlCreate);
+        statement.close();
+        connection.commit();
+    }
+}
